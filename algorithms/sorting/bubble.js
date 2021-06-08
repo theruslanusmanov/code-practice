c66d99828d1ec4c2b8237c1d2fa6f6a05f548090
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-06-08 23:16:52 +0500

    add func

---
+++ algorithms/sorting/bubble.js
@@ -0,0 +1,8 @@
+/* Bubble Sort is a algorithm to sort an array. It
+* compares adjacent element and swaps thier position
+* The big O on bubble sort in worst and best case is O(N^2).
+ * Not efficient.
+*/
+function bubbleSort() {
+
+}
