a17ecc0571392d7f13eeb6046cf56293072392e8
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-06-14 07:39:02 +0500

    add bubble sort func

--- algorithms/sorting/bubble.js
+++ algorithms/sorting/bubble.js
@@ -3,6 +3,27 @@
 * The big O on bubble sort in worst and best case is O(N^2).
  * Not efficient.
 */
-function bubbleSort() {
-
+function bubbleSort (items) {
+  const length = items.length
+  for (let i = (length - 1); i > 0; i--) {
+    // Number of passes
+    for (let j = (length - 1); j > 0; j--) {
+      // Compare the adjacent positions
+      if (items[j] < items[j - 1]) {
+        // Swap the numbers
+        [items[j], items[j - 1]] = [items[j - 1], items[j]]
+      }
+    }
+  }
 }
+
+// Implementation of bubbleSort
+
+var ar = [5, 6, 7, 8, 1, 2, 12, 14]
+// Array before Sort
+console.log('-----before sorting-----')
+console.log(ar)
+bubbleSort(ar)
+// Array after sort
+console.log('-----after sorting-----')
+console.log(ar)
