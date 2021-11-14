49769b32c6572776c4e11b379452cbae909415bd
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-11-14 11:18:00 +0500

    add valid_braces func

--- codewars/rust/src/main.rs
+++ codewars/rust/src/main.rs
@@ -1,3 +1,38 @@
+/**
+    Kata link: https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/rust
+    Examples: `
+      "(){}[]"   =>  True
+      "([{}])"   =>  True
+      "(}"       =>  False
+      "[(])"     =>  False
+      "[({})](]" =>  False
+    `
+**/
 fn main() {
     println!("Hello, world!");
+    valid_braces("(){}[]");
+}
+
+fn valid_braces(s: &str) -> bool {
+    println!("{}", s);
+
+    let mut opening_braces = Vec::new();
+    let mut closing_braces = Vec::new();
+
+    for c in s.chars() {
+        match c {
+            '{' | '(' | '[' => opening_braces.push(c),
+            '}' | ')' | ']' => closing_braces.push(c),
+            _ => {}
+        }
+    }
+
+    println!("{:?}", opening_braces);
+    println!("{:?}", closing_braces);
+
+    if opening_braces.len() != closing_braces.len() {
+        false
+    }
+
+    false
 }
