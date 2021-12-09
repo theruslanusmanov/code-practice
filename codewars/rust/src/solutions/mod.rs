2a9cbe3e030d28d80cae4f137668a54160adf348
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-09 07:37:27 +0500

    add multiple_of_3_or_5.rs

--- codewars/rust/src/solutions/mod.rs
+++ codewars/rust/src/solutions/mod.rs
@@ -1,4 +1,5 @@
+pub mod multiple_of_3_or_5;
+pub mod open_or_senior;
 pub mod reverse_words;
 pub mod valid_braces;
 pub mod vowels_count;
-pub mod open_or_senior;
---
+++ codewars/rust/src/solutions/multiple_of_3_or_5.rs
@@ -0,0 +1,29 @@
+/// Kata: https://www.codewars.com/kata/514b92a657cdc65150000006/train/rust
+fn multiple_of_3_or_5(num: i32) -> i32 {
+    let mut sum: i32 = 0;
+    let mut dividable_nums: Vec<i32> = vec![];
+
+    if num < 0 {
+        return sum;
+    }
+
+    for i in 1..num {
+        dividable_nums.push(i);
+    }
+
+    println!("{:?}", dividable_nums);
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::solutions::multiple_of_3_or_5::multiple_of_3_or_5;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(multiple_of_3_or_5(10), 23);
+        assert_eq!(multiple_of_3_or_5(11), 33);
+        assert_eq!(multiple_of_3_or_5(6), 8);
+    }
+}
