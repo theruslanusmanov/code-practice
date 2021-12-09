e5de4cfba5a6ee845100cd356a4f1f0a82a12e2a
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-09 08:50:24 +0500

    fix arguments

--- codewars/js/delete-nth/delete-nth.js
+++ codewars/js/delete-nth/delete-nth.js
@@ -1,8 +1,10 @@
 /**
  * Kata: https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
  */
-function deleteNth(fn) {
+function deleteNth(arr, n) {
+    const result = arr.reduce();
 
+    return result;
 }
 
 module.exports = deleteNth;
