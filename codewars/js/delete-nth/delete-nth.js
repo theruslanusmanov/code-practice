efce86bfbacfbea55a0009b223142164b58c5023
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-09 08:45:31 +0500

    add delete-nth.js

---
+++ codewars/js/delete-nth/delete-nth.js
@@ -0,0 +1,8 @@
+/**
+ * Kata: https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/javascript
+ */
+function deleteNth(fn) {
+
+}
+
+module.exports = deleteNth;
---
+++ codewars/js/delete-nth/delete-nth.spec.js
@@ -0,0 +1,9 @@
+const deleteNth = require('./delete-nth');
+
+describe("#deleteNth", () => {
+
+  it("test", () => {
+    expect(deleteNth([20,37,20,21], 1)).toStrictEqual([20,37,21])
+    expect(deleteNth([1,1,3,3,7,2,2,2,2], 3)).toStrictEqual([1, 1, 3, 3, 7, 2, 2, 2])
+  })
+});
