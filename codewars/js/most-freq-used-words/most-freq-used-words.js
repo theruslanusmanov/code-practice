5036998b033eabd92aa972ca8d2d96b67c9ad263
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-18 20:54:42 +0500

    set count of words

--- codewars/js/most-freq-used-words/most-freq-used-words.js
+++ codewars/js/most-freq-used-words/most-freq-used-words.js
@@ -1,7 +1,25 @@
 // https://www.codewars.com/kata/51e056fe544cf36c410000fb/train/javascript
 
 function topThreeWords(text) {
+  const splitString = text.trim().split(/\s+/);
+  const setString = new Set(splitString);
+  const mapString = new Map(setString.entries());
 
+  mapString.forEach((v, k) => {
+    mapString.set(k, 0);
+  });
+
+  console.log(mapString.entries());
+
+  splitString.map(v => {
+    mapString.forEach((_, k) => {
+      if (mapString.has(v)) {
+        mapString.set(v, mapString.get(v) + 1);
+      }
+    });
+  });
+
+  console.log(mapString.entries());
 }
 
 module.exports = topThreeWords;
