0bbafc73d70bbed0e7d15a889b5d2730d66c35bb
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-19 05:11:38 +0500

    add arrays of result

--- codewars/js/most-freq-used-words/most-freq-used-words.js
+++ codewars/js/most-freq-used-words/most-freq-used-words.js
@@ -1,15 +1,13 @@
 // https://www.codewars.com/kata/51e056fe544cf36c410000fb/train/javascript
 
 function topThreeWords(text) {
-  const splitString = text.trim().split(/\s+/);
+  const splitString = text.toLowerCase().trim().split(/\s+/);
   const setString = new Set(splitString);
   const mapString = new Map(setString.entries());
 
   mapString.forEach((v, k) => {
     mapString.set(k, 0);
   });
-
-  console.log(mapString.entries());
 
   splitString.map(v => {
     mapString.forEach((_, k) => {
@@ -19,7 +17,20 @@ function topThreeWords(text) {
     });
   });
 
-  console.log(mapString.entries());
+  console.log(mapString);
+
+  const result = new Map(
+    [...mapString.entries()]
+      .sort((a, b) => {
+        if (a[1] < b[1]) return 1;
+        else if (a[1] === b[1]) return 0;
+        else return -1;
+      })
+      .slice(0, 3)
+  );
+  console.log([...result.keys()]);
+
+  return [...result.keys()];
 }
 
 module.exports = topThreeWords;
