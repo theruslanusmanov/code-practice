0ced18144ef24fa6d7f23cee42e72986a8279301
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-04 19:23:08 +0500

    lint

--- leetcode/problems/pascals-triangle.spec.js
+++ leetcode/problems/pascals-triangle.spec.js
@@ -3,7 +3,7 @@ import {generate} from "./pascals-triangle";
 describe(`Pascal's triangle`, () => {
   it('should be right output', () => {
     const input = 5;
-    const output = [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]];
+    const output = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]];
 
     expect(generate(input)).toStrictEqual(output);
   })
