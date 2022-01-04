79ce5aabdce132fa497ea5784782ca2cfc1b4a02
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-04 15:41:35 +0500

    add pascals-triangle solution

--- leetcode/problems/pascals-triangle.js
+++ leetcode/problems/pascals-triangle.js
@@ -1,15 +1,22 @@
 /**
- * @param {number} numRows
+ * @param {number} n
  * @return {number[][]}
  */
-export const generate = function(numRows) {
-  const result = [new Array(numRows).fill(1)];
-  for (let i = 0; i < numRows; i++) {
+export const generate = (n = 0) => {
+  const result = []
+  for (let i = 0; i < n; i++) {
+    result.push(new Array(i));
+  }
+
+  for (let i = 0; i < n; i++) {
     for (let j = 0; j <= i; j++) {
-      if (j !== 0 && j !== i) {
-        result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
+      if (j === 0 || j === i) {
+        result[i][j] = 1;
+      } else {
+        result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
       }
     }
   }
+
   return result;
 };
