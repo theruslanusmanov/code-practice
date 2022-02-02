8e557a6fc74658c5988149b02c1d98d501a64557
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-02-02 21:10:06 +0500

    add must have

---
+++ base/must-have.js
@@ -0,0 +1,30 @@
+// Algorithms and Data Structures
+
+/* Data Structures */
+
+// 1. Linked Lists.
+// 2. Tree
+// 3. Tries
+// 4. Graphs
+// 5. Stacks
+// 6. Queues
+// 7. Heaps
+// 8. Vectors
+// 9. ArrayLists
+// 10. Hash Tables
+
+/* Algorithms */
+
+// 1. BFS
+// 2. DFS
+// 3. Binary Search
+// 4. Merge Sort
+// 5. Quick Sort
+
+/* Concepts */
+
+// 1. Bit Manipulation
+// 2. Memory (Stack vs Heap)
+// 3. Recursion
+// 4. Dynamic Programming
+// 5. Big O Time & Space
