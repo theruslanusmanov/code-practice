0a92fd06917d2982e939dbbde90a72e4860d92cb
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-03-09 09:44:47 +0500

    add test case

--- leetcode/problems/intersection-of-two-linked-lists.spec.js
+++ leetcode/problems/intersection-of-two-linked-lists.spec.js
@@ -1,13 +1,18 @@
-import {linkedList, ListNode} from '../structures/linked-list';
+import {linkedList} from '../structures/linked-list';
 import {getIntersectionNode} from './intersection-of-two-linked-lists';
 
 describe('Intersection of Two Linked Lists', () => {
   it('test 1', () => {
     const intersectVal = 2,
         listA = linkedList([1, 9, 1, 2, 4]),
-        listB = linkedList([3, 2, 4]),
-        output = new ListNode(2);
+        listB = linkedList([3, 2, 4]);
+    expect(getIntersectionNode(listA, listB).val).toEqual(intersectVal);
+  });
 
-    expect(getIntersectionNode(listA, listB)).toStrictEqual(output);
+  it('test 2', () => {
+    const intersectVal = 0,
+      listA = linkedList([2,6,4]),
+      listB = linkedList([1,5]);
+    expect(getIntersectionNode(listA, listB).val).toEqual(intersectVal);
   });
 });
