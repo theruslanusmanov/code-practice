547e6a73e63ec4c5346207cdad3504bc8eb4afa3
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-03-23 21:24:27 +0500

    add solution

---
+++ codewars/js/sum-arrays.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+// Sum Numbers
+function sum (numbers) {
+  "use strict";
+  return numbers.reduce((prev, curr) => prev + curr, 0);
+}
