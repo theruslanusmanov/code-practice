535e9ffa8a8a28aa1a33c28656b889695e7324c2
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-08 08:54:30 +0500

    nextPermutation

--- leetcode/js/problems/solutions.js
+++ leetcode/js/problems/solutions.js
@@ -80,13 +80,95 @@ var fourSum = function(nums, target) {
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-  let fast = head, slow = head
+  let fast = head, slow = head;
   for (let i = 0; i < n; i++) fast = fast.next;
   if (!fast) return head.next;
   while (fast.next) {
-    fast = fast.next
-    slow = slow.next
+    fast = fast.next;
+    slow = slow.next;
   }
   slow.next = slow.next.next;
   return head;
+};
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(N) {
+  let ans = [], m = 2 * N;
+
+  const dfs = (pos, open, seq) => {
+    if (pos === m) {
+      let res = new Array(m);
+      for (let i = 0; i < m; i++)
+        res[i] = seq & 1 << i ? '(' : ')';
+      ans.push(res.join(''));
+      return;
+    }
+    if (open) dfs(pos + 1, open - 1, seq);
+    if (m - pos > open) dfs(pos + 1, open + 1, seq | 1 << pos);
+  };
+
+  dfs(0, 0, 0);
+  return ans;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(data) {
+  let current = data;
+  let count = 0;
+  while (current) {
+    if (count % 2 === 0 && current.next) {
+      const tmp = current.val;
+      current.val = current.next.val;
+      current.next.val = tmp;
+    }
+    count++;
+    current = current.next;
+  }
+  return data;
+};
+
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(A, B) {
+  if (A === -2147483648 && B === -1) return 2147483647;
+  let ans = 0, sign = 1;
+  if (A < 0) A = -A, sign = -sign;
+  if (B < 0) B = -B, sign = -sign;
+  if (A === B) return sign;
+  for (let i = 0, val = B; A >= B; i = 0, val = B) {
+    while (val > 0 && val <= A) val = B << ++i;
+    A -= B << i - 1, ans += 1 << i - 1;
+  }
+  return sign < 0 ? -ans : ans;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(N) {
+  const swap = (a, b) => [N[a], N[b]] = [N[b], N[a]];
+  let len = N.length - 1, i, j;
+  for (i = len - 1; N[i] >= N[i + 1];) i--;
+  for (let k = i + 1; len > k; k++, len--) swap(k, len);
+  if (~i) {
+    for (j = i + 1; N[i] >= N[j];) j++;
+    swap(i, j);
+  }
 };
