b2922c4a1cd39120a603a20c992dcf39be6c4533
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-10 15:48:29 +0500

    min_cost_climbing_stairs

--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -5,7 +5,6 @@ fn main() {
 }
 
 pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-
     fn backtrack(sub: &[i32], candidates: &[i32], target: i32, res: &mut Vec<Vec<i32>>) {
         let sum = sub.iter().sum::<i32>();
         if sum == target {
@@ -26,4 +25,22 @@ pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
     backtrack(&vec![], &candidates, target, &mut res);
 
     res
+}
+
+pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
+    let len = cost.len();
+    // pprev that holds the cost to reach n-2 stair case
+    let mut pprev = cost[0];
+    // prev that holds the cost to reach n-1(prev) stair case
+    let mut prev = cost[1];
+
+    for stair in 2..len {
+        // the min cost to reach the current stair = current cost + min (min cost to reach n-1, min cost to reach n-2)
+        let next_prev = cost[stair] + pprev.min(prev);
+        // updating new states
+        pprev = prev;
+        prev = next_prev
+    }
+    // result = min cost of reaching n-1 step and n-2
+    pprev.min(prev)
 }
