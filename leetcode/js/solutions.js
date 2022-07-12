2f62d51c92945dc58dc78be5d34836d4f975bcc7
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-12 08:48:25 +0500

    insert

--- leetcode/js/solutions.js
+++ leetcode/js/solutions.js
@@ -590,7 +590,7 @@ var permuteUnique = function (nums) {
  * @param {number[]} matchsticks
  * @return {boolean}
  */
-var makesquare = function(matchsticks) {
+var makesquare = function (matchsticks) {
     const total = matchsticks.reduce((total, length) => total + length);
     if (total % 4 !== 0) return false;
 
@@ -617,4 +617,32 @@ var makesquare = function(matchsticks) {
 
     matchsticks.sort((a, b) => b - a);
     return dfs();
+};
+
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function (intervals, newInterval) {
+    const result = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        let interval = intervals[i];
+
+        if (Math.max(interval[0], newInterval[0]) <= Math.min(interval[1], newInterval[1])) {
+            newInterval = [Math.min(interval[0], newInterval[0]), Math.max(interval[1], newInterval[1])];
+            continue;
+        }
+
+        if (interval[0] > newInterval[1]) {
+            result.push(newInterval, ...intervals.slice(i));
+            return result;
+        }
+
+        result.push(interval);
+    }
+
+    result.push(newInterval);
+    return result;
 };
